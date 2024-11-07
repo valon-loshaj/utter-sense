74c54592b20b6d8256b87ebf160791d728b6ce90
valon <valonloshaj@gmail.com>
2024-11-06 21:54:43 -0500

    started working on the scv implementation

--- force-app/main/default/lwc/utterSenseKnowledgeRecommendation/utterSenseKnowledgeRecommendation.js
+++ force-app/main/default/lwc/utterSenseKnowledgeRecommendation/utterSenseKnowledgeRecommendation.js
@@ -1,13 +1,40 @@
 import { LightningElement, track } from "lwc";
 import getKnowledgeArticles from "@salesforce/apex/UtterSenseKnowledgeRecController.getKnowledgeArticles";
+import { getSCVToolkit } from "lightning/serviceSCV";
 
 export default class UtterSenseKnowledgeRecommendation extends LightningElement {
 	@track recommendedArticles = [];
 	@track isLoading = false; // Loading state
 	searchString = "Tesla"; // Default search string
+	scvToolkit;
 
 	connectedCallback() {
-		this.fetchKnowledgeArticles();
+		this.initializeSCVToolkit();
+	}
+
+	initializeSCVToolkit() {
+		getSCVToolkit()
+			.then((scvToolkit) => {
+				this.scvToolkit = scvToolkit;
+				this.registerTelephonyEventListeners();
+			})
+			.catch((error) => {
+				console.error("Error initializing SCV Toolkit:", error);
+				this.fetchKnowledgeArticles(); // Fetch articles with the default searchString if there's an error
+			});
+	}
+
+	registerTelephonyEventListeners() {
+		if (this.scvToolkit) {
+			this.scvToolkit.onTelephonyEvent((event) => {
+				if (event && event.type === "utterance") {
+					console.log("Utterance Event:", event);
+					console.log("Utterance Text:", event.data.utterance);
+					this.searchString = event.data.utterance || this.searchString; // Update searchString with the utterance
+					this.fetchKnowledgeArticles();
+				}
+			});
+		}
 	}
 
 	async fetchKnowledgeArticles() {
